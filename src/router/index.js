c3e467640a7a3208c90a03cb35c7fdfe73041b8c
JDaniel <daniel@JDStart.onmicrosoft.com>
2024-02-04 17:20:46 +0000

    Using Routes with metadata

--- src/router/index.js
+++ src/router/index.js
@@ -9,7 +9,15 @@ const router = createRouter({
     history: createWebHistory(),
     routes: [
         /* { path: "/home", redirect: { name: "home" } }, */
-        { path: "/", name: "home", component: HomeViewVue, alias: ["/home"] },
+        {
+            path: "/",
+            name: "home",
+            component: HomeViewVue,
+            alias: ["/home"],
+            meta: {
+                requiresAuth: false,
+            }
+        },
         { path: "/about", name: "about", component: () => import("../views/AboutView.vue") },
         {
             path: "/session",
@@ -27,6 +35,9 @@ const router = createRouter({
         {
             path: "/chats",
             component: () => import("../views/ChatsView.vue"),
+            meta: {
+                requiresAuth: true
+            },
             children: [
                 {
                     path: ":chatId",
@@ -44,6 +55,11 @@ const router = createRouter({
 
 router.beforeEach((to, from) => {
     console.log(to, from);
+
+    if (to.meta?.requiresAuth) {
+        return "/session";
+    }
+
     if (to.path === "/") {
         return { name: "about" };
     }
