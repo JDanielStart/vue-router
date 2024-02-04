12df66444b4467bf345f27809d84b3b878718aef
JDaniel <daniel@JDStart.onmicrosoft.com>
2024-02-04 11:51:39 +0000

    Using programmatic navigation

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,6 +4,20 @@
       return {
         username: ""
       }
+    },
+    methods: {
+      iniciar() {
+        //this.$router.go(-1) va hacia atrás del historial
+        //this.$router.go(2) va hacia delante del historial
+        this.$router.push({
+          path: "/chats",
+          query: {
+            variable1: 1
+          },
+          //Con esto no se guarda como punto en la historia
+          replace: true
+        });
+      }
     }
   }
 </script>
@@ -12,5 +26,6 @@
   <main>
     <h3>Introduce tú nombre de usuario</h3>
     <input placeholder="username" type="text" v-model="username">
+    <button @click="iniciar">Iniciar sesión</button>
   </main>
 </template>
